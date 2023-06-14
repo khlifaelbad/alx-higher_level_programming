52fa7323b4924a8c4298bf600d9f2b87f3e108f9
khlifaelbad <khlifaelbad@gmail.com>
2023-06-14 02:26:22 +0000

    Updated Python Data Structures

--- 0x03-python-data_structures/0-main.py
+++
@@ -1,4 +0,0 @@
-st_integer = __import__('0-print_list_integer').print_list_integer
-
-my_list = [1, 2, 3, 4, 5]
-print_list_integer(my_list)i
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++
@@ -1,6 +0,0 @@
-st_integer(my_list=[]):
-    if my_list is None:
-        return
-    for i in reversed(my_list):
-        print('{:d}'.format(i))
-
