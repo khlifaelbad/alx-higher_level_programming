35b6379496571ae3586ad88c5e17f376a7cd3571
khlifaelbad <khlifaelbad@gmail.com>
2024-02-15 02:36:50 +0100

    MORE CLASSES WITH PY

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is the "Rectangle"  module.
+
+This module provides a simple Rectangle class.
+"""
+
+
+class Rectangle:
+    """A simple Rectangle class"""
+    pass
+
