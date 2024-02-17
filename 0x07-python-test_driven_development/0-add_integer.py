f3cfaf95e5c4dea4fecf325737c4191da16888b8
khlifaelbad <khlifaelbad@gmail.com>
2024-02-17 02:37:54 +0100

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 """
-This is the "Add Integer"  module.
-
-This module supplies one function, add_integer(),
-which adds together 2 int or float types and returns an int.
+add integer
 """
 
 
-def add_integer(a, b):
-    """Return the sum of two integers or floats as an integer.
-    Otherwise raise a TypeError for given incorrect argument type.
+def add_integer(a, b=98):
     """
-    h = list(map(lambda x: isinstance(x, (int, float)), [a, b]))
-
-    if all(h):
-        return int(a) + int(b)
-
-    for x, y in list(zip(h, ['a', 'b'])):
-        if not x:
-            raise TypeError("{} must be an integer".format(y))
+    add a + b
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    if type(b) is float:
+        b = int(b)
+    return a+b
 
